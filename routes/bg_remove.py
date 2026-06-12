30fb9ced74797183ea074396d53fa4da2bc63c9e
Hassan <abdurrab.dev@gnail.com>
2026-06-13 00:24:32 +0500

    set python version for stability

--- routes/bg_remove.py
+++ routes/bg_remove.py
@@ -1,23 +1,21 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, HTTPException, UploadFile, File
 from fastapi.responses import Response
 from services.bg_service import remove_background
 
 router = APIRouter()
 
 @router.post("/remove-bg")
-async def remove_bg(request: Request):
+async def remove_bg(file: UploadFile = File(...)):
     try:
-        image_bytes = await request.body()
+        image_bytes = await file.read()
 
         if not image_bytes:
-            raise HTTPException(status_code=400, detail="No image provided")
-
-        print("IMAGE SIZE:", len(image_bytes))
+            raise HTTPException(status_code=400, detail="Empty file")
 
         output = remove_background(image_bytes)
 
         return Response(content=output, media_type="image/png")
 
     except Exception as e:
-        print("ERROR:", str(e))
+        print("FATAL ERROR:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
