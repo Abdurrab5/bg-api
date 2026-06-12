88c0003918ae78838532eee24e4eb5079433e231
Hassan <abdurrab.dev@gnail.com>
2026-06-13 00:16:07 +0500

    set python version for stability

--- routes/bg_remove.py
+++ routes/bg_remove.py
@@ -12,9 +12,12 @@ async def remove_bg(request: Request):
         if not image_bytes:
             raise HTTPException(status_code=400, detail="No image provided")
 
+        print("IMAGE SIZE:", len(image_bytes))
+
         output = remove_background(image_bytes)
 
         return Response(content=output, media_type="image/png")
 
     except Exception as e:
+        print("ERROR:", str(e))
         raise HTTPException(status_code=500, detail=str(e))
--- services/bg_service.py
+++ services/bg_service.py
@@ -1,6 +1,5 @@
 from rembg import remove, new_session
 
-session = new_session()
-
 def remove_background(image_bytes: bytes) -> bytes:
+    session = new_session()
     return remove(image_bytes, session=session)
