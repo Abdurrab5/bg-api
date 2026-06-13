b0a98eb562ce0048df42bfbbe48c3d602bd26bae
Hassan <abdurrab.dev@gnail.com>
2026-06-13 15:24:22 +0500

    set python version for stability

--- routes/bg_remove.py
+++ routes/bg_remove.py
@@ -1,21 +1,15 @@
-from fastapi import APIRouter, HTTPException, UploadFile, File
-from fastapi.responses import Response
-from services.bg_service import remove_background
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import StreamingResponse
+from services.bg_service import remove_bg
+import io
 
 router = APIRouter()
 
-@router.post("/remove-bg")
-async def remove_bg(file: UploadFile = File(...)):
-    try:
-        image_bytes = await file.read()
+@router.post("/bg-remove")
+async def bg_remove(file: UploadFile = File(...)):
+    output_image = await remove_bg(file)
 
-        if not image_bytes:
-            raise HTTPException(status_code=400, detail="Empty file")
-
-        output = remove_background(image_bytes)
-
-        return Response(content=output, media_type="image/png")
-
-    except Exception as e:
-        print("FATAL ERROR:", str(e))
-        raise HTTPException(status_code=500, detail=str(e))
+    return StreamingResponse(
+        io.BytesIO(output_image),
+        media_type="image/png"
+    )
--- services/bg_service.py
+++ services/bg_service.py
@@ -1,5 +1,7 @@
-from rembg import remove, new_session
+from rembg import remove
+from fastapi.concurrency import run_in_threadpool
 
-def remove_background(image_bytes: bytes) -> bytes:
-    session = new_session()
-    return remove(image_bytes, session=session)
+async def remove_bg(file):
+    input_image = await file.read()
+    output_image = await run_in_threadpool(remove, input_image)
+    return output_image
