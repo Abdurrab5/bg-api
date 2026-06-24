9941415643f0a1c7cac54c73147decf355d3fc82
Hassan <abdurrab.dev@gnail.com>
2026-06-24 14:56:04 +0500

    add python version3.14 new version scalable

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from routes.bg_remove import router
+from pathlib import Path
+import shutil
+import os
 
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_SRC = BASE_DIR / "models" / "u2net.onnx"
+MODEL_DST = Path("/home/user/.u2net/u2net.onnx")
+
+os.makedirs(MODEL_DST.parent, exist_ok=True)
+
+if not MODEL_DST.exists():
+    shutil.copy(MODEL_SRC, MODEL_DST)
+    
 app = FastAPI(
     title="BG Remove API",
     version="1.0"
