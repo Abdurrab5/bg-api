048b4dc75e9afbde50f4a2502f689b43f63d718e
Hassan <abdurrab.dev@gnail.com>
2026-06-24 15:40:43 +0500

    add python version3.14 new version scalable

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ from routes.bg_remove import router
 from pathlib import Path
 import shutil
 import urllib.request
-import os
 
 BASE_DIR = Path(__file__).resolve().parent
 
 MODELS_DIR = BASE_DIR / "models"
-MODEL_SRC = MODELS_DIR / "u2net.onnx"
-MODEL_DST = Path.home() / ".u2net" / "u2net.onnx"
+MODEL_SRC = MODELS_DIR / "u2netp.onnx"
+MODEL_DST = Path.home() / ".u2net" / "u2netp.onnx"
 
-MODEL_URL = "https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx"
+MODEL_URL = "https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2netp.onnx"
 
 MODELS_DIR.mkdir(parents=True, exist_ok=True)
 MODEL_DST.parent.mkdir(parents=True, exist_ok=True)
 
 try:
     if not MODEL_SRC.exists():
-        print("Downloading u2net.onnx...")
+        print("Downloading u2netp.onnx...")
         urllib.request.urlretrieve(MODEL_URL, MODEL_SRC)
         print(f"Downloaded: {MODEL_SRC}")
 
--- services/bg_service.py
+++ services/bg_service.py
@@ -1,7 +1,16 @@
-from rembg import remove
+from rembg import remove, new_session
 from fastapi.concurrency import run_in_threadpool
+
+# Load model once when app starts
+session = new_session("u2netp")
 
 async def remove_bg(file):
     input_image = await file.read()
-    output_image = await run_in_threadpool(remove, input_image)
+
+    output_image = await run_in_threadpool(
+        remove,
+        input_image,
+        session=session
+    )
+
     return output_image
