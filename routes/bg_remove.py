00832bb736e4406510aa5423d1e1b92318afabdf
Hassan <abdurrab.dev@gnail.com>
2026-06-24 17:57:48 +0500

    add python version3.14 new version scalable

--- routes/bg_remove.py
+++ routes/bg_remove.py
@@ -7,7 +7,11 @@ router = APIRouter()
 
 @router.post("/bg-remove")
 async def bg_remove(file: UploadFile = File(...)):
+    print("REQUEST RECEIVED")
+
     output_image = await remove_bg(file)
+
+    print("REMOVE_BG RETURNED")
 
     return StreamingResponse(
         io.BytesIO(output_image),
--- services/bg_service.py
+++ services/bg_service.py
@@ -1,16 +1,27 @@
 from rembg import remove, new_session
 from fastapi.concurrency import run_in_threadpool
 
-# Load model once when app starts
+print("LOADING U2NETP MODEL...")
+
 session = new_session("u2netp")
 
+print("MODEL LOADED SUCCESSFULLY")
+
 async def remove_bg(file):
+    print("START remove_bg")
+
     input_image = await file.read()
+
+    print("FILE SIZE:", len(input_image))
+
+    print("CALLING REMBG")
 
     output_image = await run_in_threadpool(
         remove,
         input_image,
         session=session
     )
+
+    print("REMBG FINISHED")
 
     return output_image
