fc237fd57e7d1789f243beb2dbf3ff8f32fbba1d
Hassan <abdurrab.dev@gnail.com>
2026-07-02 19:06:28 +0500

    New u2netp model code

--- services/bg_service.py
+++ services/bg_service.py
@@ -1,7 +1,16 @@
-from rembg import remove
+from rembg import remove, new_session
 from fastapi.concurrency import run_in_threadpool
+
+# Load model once
+session = new_session("u2netp")
 
 async def remove_bg(file):
     input_image = await file.read()
-    output_image = await run_in_threadpool(remove, input_image)
-    return output_image
+
+    output = await run_in_threadpool(
+        remove,
+        input_image,
+        session=session
+    )
+
+    return output
